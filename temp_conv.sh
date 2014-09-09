6efb52e8ad6636dfe0cd03c9ce846cff7aa0a692
mcriech <mcriech@gmail.com>
2014-09-09 10:44:17 -0500

    Updated temp conversion script output Kelvin as well

--- temp_conv.sh
+++ temp_conv.sh
@@ -2,7 +2,7 @@
 #Declare the varialbes for farenhite and celcius temps 
 #Initalize temps as ints
 
-declare -i tempF tempC
+declare -i tempF tempC tempK
 
 #If the use gave a temperature as an arguement, use it
 #Otherwise wait for an input
@@ -16,5 +16,9 @@ fi
 #Convert the temperature from F to C
 tempC=($tempF-32)/9*5
 
+#Convert C to K
+tempK=($tempC-273.15)
+
 #Print out the temperature in F (input) and C (output)
 echo "$tempF degrees F = $tempC degrees C"
+echo "$tempF degrees F = $tempK degrees K"
