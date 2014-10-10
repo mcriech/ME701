2caad78deed92ae8267577fce98046347f94610e
mcriech <mcriech@gmail.com>
2014-10-10 07:28:42 -0500

    Just updating because I havn't commited since yesterday... I think there is a new exicutible for HW3P3

--- HW3/HW3_Prob2_MP.f90
+++ HW3/HW3_Prob2_MP.f90
@@ -2,8 +2,8 @@ PROGRAM MP_Integration
  IMPLICIT NONE
 
  !Initialize variables 
- INTEGER, PARAMETER :: dimensions = 1, points = 100
- INTEGER :: dimension_1, dimension_2, point, point_2, t1, t2, clock_rate, clock_max
+ INTEGER, PARAMETER :: dimensions = 2, points = 100
+ INTEGER :: dimension_1, dimension_2, dimension_3, point, point_2, t1, t2, clock_rate, clock_max
  REAL :: z = 0.0, var = 0.0, zi, I = 0.0, sI = 0.0, x = 0.0, solution = 1.0, error, dx
  !Start the timer
  CALL system_clock(t1, clock_rate, clock_max)
@@ -14,16 +14,25 @@ PROGRAM MP_Integration
  END DO
  
  !Do the MidPoint rule approximation
+ !In general, the MidPoint method takes the value at the center point, multiplied by the
+ !differential volume about that point
+ !dx is the same for each dimension in this case since they all vary from 0 - 1
  dx = 1.0/(points)
+ !Need to find the volumes at each midpoint, for each dimension
  DO dimension_1 = 1, dimensions
-  DO point = 0, points - 1
-   zi = 1.0
-   x = point*dx + dx/2
-   DO dimension_2 = 1, dimensions
-    zi = x*zi
+ !Need to go in each dimensional direction with midpoints
+  DO dimension_2 = 1, dimensions
+ !Need to find the value at each point of integration  
+   DO point = 0, points - 1
+    zi = 1.0
+    x = point*dx + dx/2
+ !Need to calculate the actual value of x1*x2*...*xn
+    DO dimension_3 = 1, dimensions
+     zi = x*zi
+    END DO
+    zi = sqrt(zi)*(dx**dimensions)
+    I = I + zi
    END DO
-   zi = sqrt(zi)*dx
-   I = I + zi
   END DO
  END DO
 
