ee01350b9527861316cc07c8cf9afeca3a3bbe04
mcriech <mcriech@gmail.com>
2014-11-18 18:59:58 -0600

    HW5P3 code updated

---
+++ HW5/HW5_Prob3/MPI_Midpoint.py
@@ -0,0 +1,43 @@
+from mpi4py import MPI
+import numpy as np
+import time
+
+start = time.time()
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()	
+
+upper_bound = 1.0
+lower_bound = 0.0
+points = 1000.0
+
+integral = 0.0
+partial_integral = 0.0
+
+dx = (upper_bound - lower_bound)/points
+
+a = int(1 + (points / size)*rank)
+b = int((points / size)*(rank + 1))
+while a <= b:
+ x = dx*a - dx
+ a += 1
+ for j in range(int(points)):
+  y = dx*j + dx
+  partial_integral += (x + y)*dx**2
+
+if rank == 0:
+ integral += partial_integral
+ for r in range(1, size):
+  integral += comm.recv(source=r, tag=111)
+
+else:
+ comm.send(partial_integral, dest=0, tag=111)
+
+end = time.time()
+time = end - start
+
+if rank == 0:
+ print "Integral:", integral, "Time:", time, "seconds"
+
+return time
